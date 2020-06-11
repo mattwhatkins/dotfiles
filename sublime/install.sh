032a76e80d46d6a1b3e8a2a1a3bb755bbb54e995
mattwhatkins <mattwhatkins@gmail.com>
2020-06-11 09:53:23 +0100

    Adding new dotfiles

---
+++ sublime/install.sh
@@ -0,0 +1,11 @@
+  # Create Sublime Text directories as they don't exist until Sublime is opened
+mkdir -p $HOME/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\
+mkdir -p $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+
+# Install Package Control
+curl "https://packagecontrol.io/Package%20Control.sublime-package" > /tmp/Package\ Control.sublime-package
+cp -r /tmp/Package\ Control.sublime-package $HOME/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
+
+# Link the settings/theme
+ln -s $HOME/.dotfiles/sublime/Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s $HOME/.dotfiles/sublime/One\ Half\ Color\ Schemes.sublime-package $HOME/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/One\ Half\ Color\ Schemes.sublime-packages
---
+++ vscode/install.sh
@@ -0,0 +1,5 @@
+code --install-extension quandinh.onehalf-dark
+code --install-extension PKief.material-icon-theme
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+
+ln -s $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
